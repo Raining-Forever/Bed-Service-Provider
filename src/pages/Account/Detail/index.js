c8f5ced86c7cd727371801af7ce2d439257893bb
Thanakit Charoenboonvivat <54524621+m1crochip@users.noreply.github.com>
2022-03-07 17:05:43 +0700

    Account Update

--- src/pages/Account/Detail/index.js
+++ src/pages/Account/Detail/index.js
@@ -16,85 +16,156 @@ import {
 
 const { Dragger } = Upload;
 const { Option } = Select;
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 10,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 14,
+    },
+  },
+};
 
 export default function Detail() {
   return (
     <div className={styles.body}>
-      <p>ข้อมูลผู้ป่วย</p>
-      <Button type="primary">แก้ไขข้อมูล</Button>
-      <Form name="validate_other">
-        <Form.Item name="idcard" label="เลขบัตรประชาชน :">
-          <Input disabled />
-        </Form.Item>
-        <Form.Item name="bdate" label="วัน/เดือน/ปีเกิด :">
-          <Input disabled />
-        </Form.Item>
+      <Form
+        name="validate_other"
+        {...formItemLayout}
+      >
+        <div className={styles.containerinfo}>
+          <div className={styles.wrapinfo1}>
+            <Form.Item
+              name="idcard"
+              label="เลขบัตรประชาชน :"
+              rules={[
+                {
+                  type: "email",
+                  message:
+                    "The input is not valid E-mail!",
+                },
+                {
+                  required: true,
+                  message:
+                    "Please input your E-mail!",
+                },
+              ]}
+            >
+              <Input
+                className={styles.inputinfo}
+                disabled
+              />
+            </Form.Item>
+            <Form.Item
+              name="gender"
+              label="คำนำหน้า :"
+            >
+              <Select
+                placeholder=""
+                allowClear
+                disabled
+              >
+                <Option value="mr">นาย</Option>
+                <Option value="mrs">นาง</Option>
+                <Option value="ms">นางสาว</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              name="fname"
+              label="ชื่อ :"
+            >
+              <Input
+                className={styles.inputinfo}
+                disabled
+              />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              label="อีเมล :"
+            >
+              <Input
+                className={styles.inputinfo}
+                disabled
+              />
+            </Form.Item>
+            <Form.Item
+              name="weight"
+              label="น้ำหนัก :"
+            >
+              <Input
+                className={styles.inputinfo}
+                suffix="kg"
+                disabled
+              />
+            </Form.Item>
+          </div>
+          <div className={styles.wrapinfo2}>
+            <Form.Item
+              name="bdate"
+              label="วัน/เดือน/ปีเกิด :"
+            >
+              <Input
+                className={styles.inputinfo}
+                disabled
+              />
+            </Form.Item>
+            <Form.Item name="gender" label="เพศ">
+              <Radio.Group disabled>
+                <Radio value="male">ชาย</Radio>
+                <Radio value="female">หญิง</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item
+              name="lname"
+              label="นามสกุล :"
+            >
+              <Input
+                className={styles.inputinfo}
+                disabled
+              />
+            </Form.Item>
+            <Form.Item
+              name="phone"
+              label="เบอร์โทรศัพท์ :"
+            >
+              <Input
+                className={styles.inputinfo}
+                disabled
+              />
+            </Form.Item>
+            <Form.Item
+              name="heigth"
+              label="ส่วนสูง :"
+            >
+              <Input
+                className={styles.inputinfo}
+                suffix="cm"
+                disabled
+              />
+            </Form.Item>
+          </div>
+        </div>
 
-        <Form.Item name="gender" label="คำนำหน้า :">
-          <Select placeholder="" allowClear disabled>
-            <Option value="mr">นาย</Option>
-            <Option value="mrs">นาง</Option>
-            <Option value="ms">นางสาว</Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item name="fname" label="ชื่อ :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="lname" label="นามสกุล :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="email" label="อีเมล :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="phone" label="เบอร์โทรศัพท์ :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="gender" label="เพศ">
+        <Form.Item
+          name="covid-evidence"
+          label="คุณมีหลักฐานการตรวจโควิด-19 :"
+        >
           <Radio.Group disabled>
-            <Radio value="male">ชาย</Radio>
-            <Radio value="female">หญิง</Radio>
-          </Radio.Group>
-        </Form.Item>
-
-        <Form.Item name="weight" label="น้ำหนัก :">
-          <Input suffix="kg" disabled />
-        </Form.Item>
-
-        <Form.Item name="heigth" label="ส่วนสูง :">
-          <Input suffix="cm" disabled />
-        </Form.Item>
-
-        <p>ที่อยู่ที่สามารถติดต่อได้</p>
-
-        <Form.Item name="address" label="ที่อยู่ :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="province" label="จังหวัด :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="district" label="อำเภอ/เขต :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="subdistrict" label="ตำบล/แขวง :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="zipcode" label="รหัสไปรษณีย์ :">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item name="covid-evidence" label="คุณมีหลักฐานการตรวจโควิด-19 :">
-          <Radio.Group disabled>
-            <Radio value="covid-evidence-yes">มี</Radio>
-            <Radio value="covid-evidence-no">ไม่มี</Radio>
+            <Radio value="covid-evidence-yes">
+              มี
+            </Radio>
+            <Radio value="covid-evidence-no">
+              ไม่มี
+            </Radio>
           </Radio.Group>
         </Form.Item>
 
@@ -102,12 +173,23 @@ export default function Detail() {
           <Checkbox.Group disabled>
             <Row>
               <Checkbox value="rt-pcr">
-                หลักฐานการตรวจโควิด-19 ด้วยวิธีการ RT-PCR(ใบรับรองแพทย์)
+                หลักฐานการตรวจโควิด-19 ด้วยวิธีการ
+                RT-PCR(ใบรับรองแพทย์)
                 <br />
-                <Form.Item name="rt-pcr-form" label="ผลตรวจที่ได้ :">
-                  <Radio.Group disabled>
-                    <Radio value="rt-pcr-form-yes">ติดเชื้อ</Radio>
-                    <Radio value="rt-pcr-form-no">ไม่ติดเชื้่อ</Radio>
+                <Form.Item
+                  name="rt-pcr-form"
+                  label="ผลตรวจที่ได้ :"
+                >
+                  <Radio.Group
+                    disabled
+                    className={styles.radioGroup}
+                  >
+                    <Radio value="rt-pcr-form-yes">
+                      ติดเชื้อ
+                    </Radio>
+                    <Radio value="rt-pcr-form-no">
+                      ไม่ติดเชื้่อ
+                    </Radio>
                   </Radio.Group>
                 </Form.Item>
                 <Dragger>
@@ -115,38 +197,75 @@ export default function Detail() {
                     <InboxOutlined />
                   </p>
                   <p className="ant-upload-text">
-                    Click or drag file to this area to upload
+                    Click or drag file to this
+                    area to upload
                   </p>
                   <p className="ant-upload-hint">
-                    Support for a single or bulk upload. Strictly prohibit from
-                    uploading company data or other band files
+                    Support for a single or bulk
+                    upload. Strictly prohibit from
+                    uploading company data or
+                    other band files
                   </p>
                 </Dragger>
               </Checkbox>
               <Checkbox value="atk">
-                หลักฐานการตรวจโควิด-19 ด้วยวิธีการ ATK
+                หลักฐานการตรวจโควิด-19 ด้วยวิธีการ
+                ATK
                 <br />
-                <Form.Item name="atk-form" label="ผลตรวจที่ได้ :">
+                <Form.Item
+                  name="atk-form"
+                  label="ผลตรวจที่ได้ :"
+                >
                   <Radio.Group disabled>
-                    <Radio value="atk-form-yes">ติดเชื้อ</Radio>
-                    <Radio value="atk-form-no">ไม่ติดเชื้่อ</Radio>
+                    <Radio value="atk-form-yes">
+                      ติดเชื้อ
+                    </Radio>
+                    <Radio value="atk-form-no">
+                      ไม่ติดเชื้่อ
+                    </Radio>
                   </Radio.Group>
                 </Form.Item>
-                <Dragger>
-                  <p className="ant-upload-drag-icon">
-                    <InboxOutlined />
-                  </p>
-                  <p className="ant-upload-text">
-                    Click or drag file to this area to upload
-                  </p>
-                  <p className="ant-upload-hint">
-                    Support for a single or bulk upload. Strictly prohibit from
-                    uploading company data or other band files
-                  </p>
-                </Dragger>
               </Checkbox>
             </Row>
           </Checkbox.Group>
+        </Form.Item>
+        <div className={styles.adresstitle}>
+          ที่อยู่ที่สามารถติดต่อได้
+        </div>
+
+        <Form.Item
+          name="address"
+          label="ที่อยู่ :"
+        >
+          <Input disabled />
+        </Form.Item>
+
+        <Form.Item
+          name="province"
+          label="จังหวัด :"
+        >
+          <Input disabled />
+        </Form.Item>
+
+        <Form.Item
+          name="district"
+          label="อำเภอ/เขต :"
+        >
+          <Input disabled />
+        </Form.Item>
+
+        <Form.Item
+          name="subdistrict"
+          label="ตำบล/แขวง :"
+        >
+          <Input disabled />
+        </Form.Item>
+
+        <Form.Item
+          name="zipcode"
+          label="รหัสไปรษณีย์ :"
+        >
+          <Input disabled />
         </Form.Item>
       </Form>
     </div>
--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -4,11 +4,23 @@ import styles from "./Account.module.css";
 import Navbar_patient from "../../components/Navbar/Navbar_patient";
 
 import Detail from "./Detail";
+import { Button } from "antd";
 
 export default function Account() {
   return (
     <div className={styles.container}>
       <div className={styles.body}>
+        <div className={styles.wrapheader}>
+          <div className={styles.header}>
+            ข้อมูลผู้ป่วย
+          </div>
+          <Button
+            type="primary"
+            htmlType="submit"
+          >
+            แก้ไขข้อมูล
+          </Button>
+        </div>
         <div className={styles.box}>
           <Detail />
         </div>
