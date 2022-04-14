06e14ec8237740653008f4dbd92e8e1e16c237b5
Thanakit Charoenboonvivat <54524621+m1crochip@users.noreply.github.com>
2022-04-14 22:35:48 +0700

    edit page appointdetail

--- src/components/AppointmentDetail/index.js
+++ src/components/AppointmentDetail/index.js
@@ -14,15 +14,31 @@ export default function AppointmentDetail() {
           <div className={styles.description}>
             <img
               className={styles.imgaccessdeined}
-              src="https://www.img.in.th/image/7RKVLM"
+              src="https://cdn-icons-png.flaticon.com/512/6400/6400498.png"
             />
-            <div>
+            <div className={styles.destext}>
               ในการพบแพทย์ออนไลน์
               กรุณาเตรียมความพร้อมกับอุปกรณ์ของท่านก่อนถึงเวลาให้คำปรึกษา
             </div>
           </div>
-          <div className={styles.description}>
-            2
+          <div className={styles.appdetail}>
+            <div className={styles.wraptext}>
+              <div className={styles.text}>
+                แพทย์ผู้ให้คำปรึกษา :
+                {"  นพ. ขยัน เก่งมาก"}
+              </div>
+              <div className={styles.text}>
+                วันที่ปรึกษา : {"1/9/2564"}
+              </div>
+              <div className={styles.text}>
+                ช่วงเวลาปรึกษา :{"15.00-15.30"}
+              </div>
+            </div>
+            <div>
+              <Button type="primary">
+                เข้าห้องประชุม
+              </Button>
+            </div>
           </div>
         </div>
       </div>
