6e90feb2e0dfab3b2f10663410d62359d7da1a82
Donutz <do12nut06@gmail.com>
2022-04-23 20:29:01 +0700

    prepare appoint detail

--- src/components/AppointmentDetail/index.js
+++ src/components/AppointmentDetail/index.js
@@ -2,14 +2,32 @@ import React from "react";
 import styles from "./AppointmentDetail.module.css";
 import { Button } from "antd";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState, useParams } from "react";
+import { Oval } from "react-loader-spinner";
+import axios from "axios";
+import { useAuthContext } from "../../context/AuthContext";
 
-export default function AppointmentDetail() {
+export default function AppointmentDetail(props) {
+  const { auth, authLoaded, roleCheck } = useAuthContext();
+  useEffect(() => {
+    roleCheck(["patient"], "/accessdenied");
+  }, [authLoaded]);
+
+  let nArray = [];
+  async function fetchDetail() {
+    if (auth.user_info?.id) {
+    }
+  }
+
+  useEffect(() => {
+    if (authLoaded) {
+      fetchDetail();
+    }
+  }, [authLoaded]);
   return (
     <div className={styles.container}>
       <div className={styles.body}>
-        <div className={styles.header}>
-          รายละเอียดการปรึกษาแพทย์
-        </div>
+        <div className={styles.header}>รายละเอียดการปรึกษาแพทย์</div>
         <div className={styles.box}>
           <div className={styles.description}>
             <img
@@ -24,20 +42,13 @@ export default function AppointmentDetail() {
           <div className={styles.appdetail}>
             <div className={styles.wraptext}>
               <div className={styles.text}>
-                แพทย์ผู้ให้คำปรึกษา :
-                {"  นพ. ขยัน เก่งมาก"}
+                แพทย์ผู้ให้คำปรึกษา :{"  นพ. ขยัน เก่งมาก"}
               </div>
-              <div className={styles.text}>
-                วันที่ปรึกษา : {"1/9/2564"}
-              </div>
-              <div className={styles.text}>
-                ช่วงเวลาปรึกษา :{"15.00-15.30"}
-              </div>
+              <div className={styles.text}>วันที่ปรึกษา : {"1/9/2564"}</div>
+              <div className={styles.text}>ช่วงเวลาปรึกษา :{"15.00-15.30"}</div>
             </div>
             <div>
-              <Button type="primary">
-                เข้าห้องประชุม
-              </Button>
+              <Button type="primary">เข้าห้องประชุม</Button>
             </div>
           </div>
         </div>
