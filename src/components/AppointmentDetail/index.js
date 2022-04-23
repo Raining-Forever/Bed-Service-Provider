f5abcb89a0707be6b74bc6f155a9df6ee088cbe1
Thanakit Charoenboonvivat <54524621+m1crochip@users.noreply.github.com>
2022-04-23 20:46:29 +0700

    appointmentDetail fetch data

--- src/components/AppointmentDetail/index.js
+++ src/components/AppointmentDetail/index.js
@@ -1,21 +1,34 @@
 import React from "react";
 import styles from "./AppointmentDetail.module.css";
 import { Button } from "antd";
-import { NavLink } from "react-router-dom";
-import { useEffect, useState, useParams } from "react";
+import {
+  NavLink,
+  useParams,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
 import { Oval } from "react-loader-spinner";
 import axios from "axios";
 import { useAuthContext } from "../../context/AuthContext";
 
 export default function AppointmentDetail(props) {
-  const { auth, authLoaded, roleCheck } = useAuthContext();
+  const { auth, authLoaded, roleCheck } =
+    useAuthContext();
+  const [appointdetail, setAppointdetail] =
+    useState({});
+
   useEffect(() => {
     roleCheck(["patient"], "/accessdenied");
   }, [authLoaded]);
 
-  let nArray = [];
+  let { id } = useParams();
+
   async function fetchDetail() {
     if (auth.user_info?.id) {
+      const result = await axios.get(
+        `https://bed-service-provider.herokuapp.com/api/appointment/${id}`
+      );
+      setAppointdetail(result.data[0]);
+      console.log(result);
     }
   }
 
@@ -27,7 +40,9 @@ export default function AppointmentDetail(props) {
   return (
     <div className={styles.container}>
       <div className={styles.body}>
-        <div className={styles.header}>รายละเอียดการปรึกษาแพทย์</div>
+        <div className={styles.header}>
+          รายละเอียดการปรึกษาแพทย์
+        </div>
         <div className={styles.box}>
           <div className={styles.description}>
             <img
@@ -42,13 +57,20 @@ export default function AppointmentDetail(props) {
           <div className={styles.appdetail}>
             <div className={styles.wraptext}>
               <div className={styles.text}>
-                แพทย์ผู้ให้คำปรึกษา :{"  นพ. ขยัน เก่งมาก"}
+                แพทย์ผู้ให้คำปรึกษา :
+                {"  นพ. ขยัน เก่งมาก"}
               </div>
-              <div className={styles.text}>วันที่ปรึกษา : {"1/9/2564"}</div>
-              <div className={styles.text}>ช่วงเวลาปรึกษา :{"15.00-15.30"}</div>
+              <div className={styles.text}>
+                วันที่ปรึกษา : {"1/9/2564"}
+              </div>
+              <div className={styles.text}>
+                ช่วงเวลาปรึกษา :{"15.00-15.30"}
+              </div>
             </div>
             <div>
-              <Button type="primary">เข้าห้องประชุม</Button>
+              <Button type="primary">
+                เข้าห้องประชุม
+              </Button>
             </div>
           </div>
         </div>
