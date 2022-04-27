3e23c89aee38eb3b466c538b9e0debe52a893d94
Donutz <do12nut06@gmail.com>
2022-04-27 22:43:45 +0700

    admin page 85%

--- src/pages/Admin/CheckEvid/index.js
+++ src/pages/Admin/CheckEvid/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./CheckEvid.module.css";
-import { Button, Table, Tag, Space } from "antd";
+import { Table, Space } from "antd";
 
 export default function CheckEvid() {
   const columns = [
